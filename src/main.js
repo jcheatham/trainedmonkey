ce42c97334e79cebaa98ead35cf2668793edb42e
Steve Goldsmith <steve@thisisstevegoldsmith.com>
2015-01-24 11:01:11 -0800

    Draw monkey.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-var trained_monkey = (function() {
+var trainedmonkey = (function() {
   var result = {};
 
-  result.init = function (loadImage) {
-    loadImage('monkey', 'img/monkey.png');
-    loadImage('item', 'img/item.png');
-    loadImage('train', 'img/train.png');
+
+  result.loadAssets = function () {
+    game.load.image('monkey', 'img/monkey.png');
+    // loadImage('item', 'img/item.png');
+    // loadImage('train', 'img/train.png');
+  }
+
+  result.init = function() {
+
+    var monkeySprite = game.add.sprite(game.world.centerX, game.world.centerY, 'monkey');
+    monkeySprite.anchor.setTo(0.5, 0.5);
+
+  }
+
+  result.update = function() {
+    
   }
 
   return result;
