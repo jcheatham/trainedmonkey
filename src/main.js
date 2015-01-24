0e61280603641048e2078cc5698b858e945f0c83
Steve Goldsmith <steve@thisisstevegoldsmith.com>
2015-01-24 11:25:48 -0800

    Underscore. Audio.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var trainedmonkey = (function() {
 
   result.loadAssets = function () {
     game.load.image('monkey', 'img/monkey.png');
+    game.load.audio('jump', 'audio/train0.wav');  
     // loadImage('item', 'img/item.png');
     // loadImage('train', 'img/train.png');
   }
@@ -13,10 +14,17 @@ var trainedmonkey = (function() {
     result.monkeySprite = game.add.sprite(game.world.centerX, game.world.centerY, 'monkey');
     result.monkeySprite.anchor.setTo(0.5, 0.5);
 
+    result.jumpSound = game.add.audio('jump');  
+
+
   }
 
   result.update = function() {
     result.monkeySprite.x += 1;
+
+    if(_.random(1000) < 3) {
+      result.jumpSound.play();
+    }
   }
 
   return result;
