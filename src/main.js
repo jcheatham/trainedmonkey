41d198cc2cd69193f57c395fadb852141ff5d3f1
Steve Goldsmith <steve@thisisstevegoldsmith.com>
2015-01-24 12:34:18 -0800

    Keyboard input and text.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var trainedmonkey = (function() {
   var result = {};
 
 
+
   result.loadAssets = function () {
 
     // monkey.loadAssets();
@@ -17,13 +18,19 @@ var trainedmonkey = (function() {
     result.monkeySprite = game.add.sprite(game.world.centerX, game.world.centerY, 'monkey');
     result.monkeySprite.anchor.setTo(0.5, 0.5);
 
-    result.jumpSound = game.add.audio('jump');  
+    result.time = 60;
+    result.clock = game.add.text(game.world.centerX, game.world.centerY + 200, result.time.toString());
 
+    result.jumpSound = game.add.audio('jump');  
 
 
   }
 
   result.update = function() {
+
+    result.time -= 1.0 / 60;
+    result.clock.text = result.time.toString();
+
     if(game.input.keyboard.isDown(39)) {  
       result.monkeySprite.x -= 1;
     } else {
