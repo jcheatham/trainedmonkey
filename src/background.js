783a43e4a51975260abd98fc877cd1a423065a7b
Anastasi Bakolias <s.bakolias@gmail.com>
2015-01-25 07:37:04 -0800

    the best tree algorithm of all time

--- src/background.js
+++ src/background.js
@@ -1,20 +1,38 @@
 var background = (function(){
+
   var results = {};
 
   results.loadAssets = function() {
     phaser.load.image('mountain', 'img/bg_1.png');
+    phaser.load.image('tree1', 'img/tree_0.png');
+    phaser.load.image('tree2', 'img/tree_1.png');
   };
 
   results.init = function() {
+    
     results.mountains = [];
     for(var i = 0; i < 5; i++){
       results.mountains.push(phaser.add.tileSprite(i*1912, 0, 1912, 600, 'mountain'));
     }
+
+    results.trees = [];
+    for(var i = 0; i < 30; i++){
+      results.trees.push(phaser.add.sprite(i*Math.floor(Math.random()*600), Math.floor(Math.random()*200), 'tree2'));
+      results.trees.push(phaser.add.sprite(i*Math.floor(Math.random()*600), Math.floor(Math.random()*200), 'tree1'));
+    }
   };
 
   results.update = function() {
+
+    _.each(results.trees, function(tree){
+      if (tree.x < -300) {
+        tree.x = phaser._width * 7;
+      }
+      tree.x -= 20;
+
+    });
     _.each(results.mountains, function(mountain){
-      mountain.tilePosition.x -= 1;
+      mountain.tilePosition.x -= .5;
     });
   };
 
--- src/gumballmachine.js
+++ src/gumballmachine.js
@@ -6,7 +6,7 @@ items["gumballMachine"] = {
   init: function(phaser) {
     this.sprite = phaser.add.sprite(3695, 265, 'gumballMachine');
     this.sprite.anchor.setTo(0.5, 0.5);
-    this.sprite.z = 100;
+    this.sprite.z = 90;
     this.sprite.scale = new PIXI.Point(8, 8);
     this.interactRect = new Phaser.Rectangle(-10,-20,20,40);
   }
