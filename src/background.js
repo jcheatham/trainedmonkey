aedac0d5fd59b65f3af1c568e95035966d3bf018
Anastasi Bakolias <s.bakolias@gmail.com>
2015-01-24 17:43:52 -0800

    made speed of background depend on distance of background layer from train

--- src/background.js
+++ src/background.js
@@ -5,14 +5,16 @@ var background = (function(){
   //class for background tracks that contain a list of sprites
   //each sprite to be iterated through on each game loop
   //updating the sprites position and resetting it if it passes off the screen
-  var SpriteTrack = function(speed){
-    this.speed = speed;
+  var SpriteTrack = function(distance){  
+    this.distance = distance;
+    this.speed = Math.floor(1000/distance);
     this.feed = [];
     this.pixelLength = 0;
     return this;
   };
 
   SpriteTrack.prototype.add = function(sprite){
+    sprite.z = (- this.distance);
     this.feed.push(sprite);
     this.pixelLength += sprite._width;
     return this;
@@ -21,6 +23,7 @@ var background = (function(){
   SpriteTrack.prototype.iterate = function(){
     var _this = this;
     this.feed.forEach(function(sprite){
+      console.log(sprite.z);
       if (sprite.x === phaser._width) {
         sprite.x = (sprite._width - _this.pixelLength);
       }
@@ -41,13 +44,13 @@ var background = (function(){
     //initialize mountainclouds
     var bg11 = phaser.add.tileSprite(0, 300, 800, 300, 'bg1'),
         bg12 = phaser.add.tileSprite(-phaser._width, 300, 800, 300, 'bg1');
-    results.mountainTrack = new SpriteTrack(1);
+    results.mountainTrack = new SpriteTrack(1000);
     results.mountainTrack.add(bg11).add(bg12);
 
     //initialize cloudclouds
     var bg21 = phaser.add.tileSprite(0, 0, 800, 300, 'bg2'),
         bg22 = phaser.add.tileSprite(-phaser._width, 0, 800, 300, 'bg2');
-    results.cloudTrack = new SpriteTrack(5);
+    results.cloudTrack = new SpriteTrack(500);
     results.cloudTrack.add(bg21).add(bg22);
   };
 
