f8e5553ebb7ea611387966c95a3a7ecbf453cec5
Jonathan Cheatham <jcheatham@zendesk.com>
2015-01-25 04:59:39 -0800

    Place wrench

--- src/wrench.js
+++ src/wrench.js
@@ -4,9 +4,10 @@ items["wrench"] = {
     phaser.load.image('wrench', 'img/wrench.png');
   },
   init: function(phaser) {
-    this.sprite = phaser.add.sprite(500, 350, 'wrench');
+    this.sprite = phaser.add.sprite(4900, 350, 'wrench');
     this.sprite.anchor.setTo(0.5, 0.5);
     this.sprite.z = 100;
+    this.sprite.scale = new PIXI.Point(4, 4);
     this.interactRect = new Phaser.Rectangle(-10,-20,20,40);
   }
 };
