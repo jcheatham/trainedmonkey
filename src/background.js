b886451afeb912911f4e1fc1b0e619b72cd9d1de
Jonathan Cheatham <jcheatham@zendesk.com>
2015-01-24 17:03:44 -0800

    Fix a few merge related bugs

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 
 var background = (function(){
 
-  //class for background tracks that contain a list of sprites 
+  //class for background tracks that contain a list of sprites
   //each sprite to be iterated through on each game loop
   //updating the sprites position and resetting it if it passes off the screen
   var SpriteTrack = function(speed){
@@ -22,7 +22,7 @@ var background = (function(){
     var _this = this;
     console.log(_this);
     this.feed.forEach(function(sprite){
-      if (sprite.x === game._width) {
+      if (sprite.x === phaser._width) {
         sprite.x = (sprite._width - _this.pixelLength);
       }
       sprite.x += _this.speed;
@@ -32,22 +32,22 @@ var background = (function(){
   var results = {};
 
   results.loadAssets = function () {
-    game.load.image('bg1', 'img/bg1.png');
-    game.load.image('bg2', 'img/bg2.png');
+    phaser.load.image('bg1', 'img/bg1.png');
+    phaser.load.image('bg2', 'img/bg2.png');
 
   };
 
   results.init = function() {
 
     //initialize mountainclouds
-    var bg11 = game.add.tileSprite(0, 300, 800, 300, 'bg1'),
-        bg12 = game.add.tileSprite(-game._width, 300, 800, 300, 'bg1');
+    var bg11 = phaser.add.tileSprite(0, 300, 800, 300, 'bg1'),
+        bg12 = phaser.add.tileSprite(-phaser._width, 300, 800, 300, 'bg1');
     results.mountainTrack = new SpriteTrack(1);
     results.mountainTrack.add(bg11).add(bg12);
 
     //initialize cloudclouds
-    var bg21 = game.add.tileSprite(0, 0, 800, 300, 'bg2'),
-        bg22 = game.add.tileSprite(-game._width, 0, 800, 300, 'bg2');
+    var bg21 = phaser.add.tileSprite(0, 0, 800, 300, 'bg2'),
+        bg22 = phaser.add.tileSprite(-phaser._width, 0, 800, 300, 'bg2');
     results.cloudTrack = new SpriteTrack(5);
     results.cloudTrack.add(bg21).add(bg22);
   };
